bfd7edf55f0ecf348a20d7c14ba292a55b4de522
Bailey Stoner <monokrome@monokro.me>
2023-01-04 10:20:40 -0800

    Colortil configuration

---
+++ plugin/colors.lua
@@ -0,0 +1,22 @@
+require("colortils").setup({
+    register = "+",
+    color_preview =  " %s",
+    default_format = "hex",
+    border = "none",
+    mappings = {
+        increment = "l",
+        decrement = "h",
+        increment_big = "L",
+        decrement_big = "H",
+        min_value = "0",
+        max_value = "$",
+        set_register_default_format = "<cr>",
+        set_register_cjoose_format = "g<cr>",
+        replace_default_format = "<m-cr>",
+        replace_choose_format = "g<m-cr>",
+        export = "E",
+        set_value = "c",
+        transparency = "T",
+        choose_background = "B",
+    }
+})
